c0b8e141013e65b01fe14ee03dcc853118aadc12
sethripman <sethripman@gmail.com>
2019-09-19 20:55:49 -0700

    web page and working is even function

---
+++ is-even.js
@@ -0,0 +1,14 @@
+function isEven(number) {
+    const test = number % 2;
+   /* if (test === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    */
+    return !test;
+}
+
+export default isEven;
+
